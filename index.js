8cc2b0dde1178a3849ebf1ecf5b63fb82819b2ba
Tanattha <tanattha.thuenim@gmail.com>
2020-07-23 20:42:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your functions here
+
+//map returns an array with operations
+   
+function map(src, operations) {
+    let r = []
+    for (let i = 0; i < src.length; i++) {
+      //let theElement = src[i]
+      r.push(operations(src[i]))
+    }
+    return r;
+  }
+
+  //returns a running total when given or not a starting point
+  //returns true when all values are true
+  //returns false when any value is false
+
+  function reduce(src, conditions, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = conditions(src[i], r)
+    }
+  
+    return r;
+  }
